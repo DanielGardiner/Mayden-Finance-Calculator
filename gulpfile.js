eb52fd4d9cec30fce673db05a3f353cc08d906a4
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-17 15:32:03 +0100

    initial commit of draft layout

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript')
+var tsProject = ts.createProject('tsconfig.json')
+
+
+function sassCompile(cb) {
+    return gulp.src('app/scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'));
+    cb();
+}
+
+function typescript(cb) {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('app/js/dist'));
+    cb();
+}
+
+function watch() {
+    gulp.watch('app/scss/**/*.scss', sassCompile);
+    gulp.watch('app/js/*.ts', typescript);
+}
+
+exports.sass = sassCompile;
+
+exports.ts = typescript;
+
+exports.watch = watch;
